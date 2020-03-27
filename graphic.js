62fed655908193b96e839af3346d26a9bdc9549b
A1RKING <60095325+A1RKING@users.noreply.github.com>
2020-03-27 14:28:38 +0300

    add files
    first

---
+++ graphic.js
@@ -0,0 +1,9 @@
+let dayOff = prompt("Введи дату, что бы узнать выходной Антон или нет (Формат даты 'xx.xx')", '');
+
+if (dayOff == "29.03" || dayOff == "30.03" || dayOff == "02.04" || dayOff == "03.04" || dayOff == "06.04" || dayOff == "07.04" || dayOff == "10.04" || dayOff == "11.04" || dayOff == "14.04" || dayOff == "15.04" || dayOff == "18.04" || dayOff == "19.04" || dayOff == "22.04" || dayOff == "23.04" || dayOff == "26.04" || dayOff == "27.04" || dayOff == "30.04" || dayOff == "01.05" || dayOff == "04.05" || dayOff == "05.05" || dayOff == "08.05" || dayOff == "09.05" || dayOff == "12.05" || dayOff == "13.05" || dayOff == "16.05" || dayOff == "17.05" || dayOff == "20.05" || dayOff == "21.05" || dayOff == "24.05" || dayOff == "25.05" || dayOff == "28.05" || dayOff == "29.05" || dayOff == "01.06" || dayOff == "02.06") {
+   alert("ВЫХОДНОЙ!!!")
+} else if (dayOff == "05.06" || dayOff == "06.06" || dayOff == "07.06" || dayOff == "08.06" || dayOff == "09.06" || dayOff == "10.06" || dayOff == "11.06" || dayOff == "11.06" || dayOff == "12.06" || dayOff == "13.06" || dayOff == "14.06" || dayOff == "15.06" || dayOff == "16.06" || dayOff == "17.06" || dayOff == "18.06") {
+   alert("ОТПУСК СУКА УААААА АУФ")
+} else {
+   alert("Рабочий день")
+}
